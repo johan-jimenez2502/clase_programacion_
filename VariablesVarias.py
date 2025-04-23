0388f8a71d46bcb6b03b9ba1d20e91b172a9e3c8
Mona Lisa <Mona Lisa>
2025-04-23 12:47:39 -0500

    segunda version

---
+++ VariablesVarias.py
@@ -0,0 +1,11 @@
+def area_rectangulo (l,a):
+    area = l * a 
+    return area
+
+largo = float(input("largo del rectanguo:"))
+ancho = float(input("ancho del rectangulo"))
+print("El area del rectanguloes:", end = "")
+print(area_rectangulo(largo, ancho))
+
+print 
+
